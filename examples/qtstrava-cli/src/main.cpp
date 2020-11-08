b59b6375c3cf9ad9071eb88109985fe10aea9513
Dmitriy Purgin <dpurgin@gmail.com>
2020-11-08 10:31:17 +0100

    License notices added

--- examples/qtstrava-cli/src/main.cpp
+++ examples/qtstrava-cli/src/main.cpp
@@ -1,3 +1,19 @@
+// qtstrava-cli - An example usage of QtStrava, a Qt wrapper for Strava API
+// Copyright (C) 2020 Dmitriy Purgin  <dpurgin@gmail.com>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 #include <QGuiApplication>
 
 #include <QtStrava/Model/detailedathlete.h>
@@ -12,6 +28,11 @@
 int main(int argc, char *argv[])
 {
     QGuiApplication app{argc, argv};
+
+    qInfo() << "qtstrava-cli  Copyright (C) 2020  Dmitriy Purgin <dpurgin@gmail.com>";
+    qInfo() << "This program comes with ABSOLUTELY NO WARRANTY. This is free software, and you are "
+               "welcome to redistribute it under certain conditions; See GNU General Public "
+               "License v3 at https://www.gnu.org/licenses/ for details.";
 
     AuthorizationHandler authorizationHandler;
 
